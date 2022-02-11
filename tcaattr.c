cc308d08725fc60d80b9bbb00aec0c3677f79ba4
gustave mary <gustave1mary@gmail.com>
2022-02-11 16:17:21 +0100

    recherche 10

--- tcaattr.c
+++ tcaattr.c
@@ -1,14 +1,18 @@
 #include "function.h"
 
+
+//petit pgm pour etre en raw mode et voir ce que lon ecrit byte par byte
+
 void enableRawMode() {
   struct termios raw;
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+      printf("%c [%d]\n", c, c);
   return 0;
 }
