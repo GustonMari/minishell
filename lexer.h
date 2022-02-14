6e571e99b38b8e0aec4cb675d3ca87482ebc3ca8
gustave mary <gustave1mary@gmail.com>
2022-02-14 12:28:12 +0100

    recherche 10

---
+++ lexer.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 11:14:36 by gmary             #+#    #+#             */
+/*   Updated: 2022/02/14 12:27:49 by gmary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+typedef enum s_tokentype
+{
+	NO_QUOTE,
+	QUOTE,
+	DQUOTE,
+	CMD,
+	DOLLAR,
+	SPACE
+}	t_tokentype;
+
+typedef struct t_token
+{
+	t_tokentype	type;
+	char		*value;
+	t_token		*next;
+}				s_token;
+
+
+#endif
--- tcaattr.c
+++ tcaattr.c
@@ -2,11 +2,18 @@
 
 
 //petit pgm pour etre en raw mode et voir ce que lon ecrit byte par byte
+// le flag ISIG permet de bloquer les signaux (gestion des ctrl)
+// le I devant ISIG et ICANON provient de INPUT
+//OPOST enleve le retour charriot pour le retablir il faudrait mettre \r avant le \n
+//When BRKINT is turned on, a break condition will cause a SIGINT signal to be sent to the
+//program, like pressing Ctrl-C. => pas trop compris linteret
 
 void enableRawMode() {
   struct termios raw;
   tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO | ICANON);
+  //raw.c_oflag &= ~(OPOST);
+  raw.c_iflag &= ~(BRKINT);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
