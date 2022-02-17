a9266448543c4fd90cc4180e2def10ad68f7e1dc
GustonMari <gustave1mary@gmail.com>
2022-02-17 18:31:34 +0100

    fix exec, need to norm

--- exec.c
+++ exec.c
@@ -6,58 +6,76 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 14:00:48 by gmary             #+#    #+#             */
-/*   Updated: 2022/02/17 17:53:36 by gmary            ###   ########.fr       */
+/*   Updated: 2022/02/17 18:28:37 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "function.h"
 
-char	*find_path_cmd(char **env, char *cmd)
+char	*find_path_cmd(char *cmd, char *tmp)
 {
 	int		i;
 	char	*path;
-	char	*new_cmd;
 	char	**all_cmd_path;
 
-	new_cmd = ft_strjoin("/", cmd);
-	if (!new_cmd)
-		return (NULL);
-	all_cmd_path = ft_split(find_val_in_tab(env, "PATH"), ':');
+
+	all_cmd_path = ft_split(tmp, ':');
 	if (!all_cmd_path)
 		return (NULL);
+	free(tmp);
 	i = 0;
 	while (all_cmd_path[i++])
 	{
-		path = ft_strjoin(all_cmd_path[i], new_cmd);
+		path = ft_strjoin(all_cmd_path[i], cmd);
 		if (access(path, F_OK | X_OK) == 0)
 		{
-			free(new_cmd);
+			free(cmd);
 			ft_free_tab_2d(all_cmd_path);
 			return (path);
 		}
 		free(path);
 	}
-	free(new_cmd);
+	free(cmd);
 	ft_free_tab_2d(all_cmd_path);
 	return (NULL);
 }
 
-int ft_exec_cmd()
+int ft_exec_cmd(char **env, char *cmd)
 {
+	char	*new_cmd;
+	char	*path;
+	char	*cmd_null[2];
+	char	*tmp;
+
+	tmp = find_val_in_tab(env, "PATH");
+	if (!tmp)
+		return (-1);
+	cmd_null[0] = ft_strdup(cmd);
+	if (!cmd_null[0])
+		return (-1);
+	cmd_null[1] = NULL;
+	new_cmd = ft_strjoin("/", cmd);
+	if (!new_cmd)
+		return (-1);
+	path = find_path_cmd(new_cmd, tmp);
+	if(execve(path, cmd_null, env) < 0)
+		perror("execve");
+	free(cmd_null[0]);
+	free(path);
 	return (0);
 }
+
+
 
 int	main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
-	char	*path;
-	char **test = malloc (2 * sizeof(char *));
+	/* char **test = malloc (2 * sizeof(char *));
 	test[0] = "ls";
 	test[1] = NULL;
-	path = find_path_cmd(env, "ls");
-	printf("path = \"%s\"\n", path);
-	if(execve(path, test, env) < 0)
-		perror("execve");
+	 */
+	ft_exec_cmd(env, "ls");
+
 	return (0);
 }
