75af3b68b2dbfb7bebbb2b234b43686b8009332a
gustave mary <gustave1mary@gmail.com>
2022-03-18 15:26:02 +0100

    mv word segfault in one case

--- lexer/remix_lexer.c
+++ lexer/remix_lexer.c
@@ -26,35 +26,39 @@ int	count_word_btw_two_op(t_token *all)
 /*
 	permet de bouger le mot apres le CHV_L
 	on est dans le cas wc < ok -l
+
+	ATTENTION NE MARCHE PAS ENCORE POUR LE CAS < ok wc
 */
 
 void	mv_word_left(t_token *all)
 {
-	printf("------- BEFORE -----------\n");
-	print_token(&all);
-	printf("------------------\n");
+
 	t_token	*tmp = NULL;
 	t_token	*forward = NULL;
 	t_token	*word = NULL;
-	//t_token	*previous = NULL;
-	//t_token	*next = NULL;
-	//foward pointe sur -l
+
+	//all serra ici toujours "<"
 	tmp = all;
-	forward = tmp->next->next->next;
+	//foward pointe sur -l
+	forward = tmp->next->next;
 	// word sur ok
-	word = tmp->next->next;
-		// tmp est sur le chevron et on veut link le chevron et le WORD + 1 ex: < ok -l on link < et -l
-	tmp = tmp->next;
-	//previous = tmp;
+	word = tmp->next;
+	// tmp est sur le chevron et on veut link le chevron et le WORD + 1 ex: < ok -l on link < et -l
+	//if (tmp->type != CHV_L)
+	printf("tmp %s\n", tmp->content);
+
+	// tmp = tmp->next;
 	// on link le chv_l et -l
+	printf("forward %s\n", tmp->content);
+
 	tmp->next = forward;
-	tmp = tmp->next;
-	printf(" .  %s\n", tmp->content);
+	//if (tmp-> type != CHV_L)
+	tmp = tmp->next->next;
+	printf("tmp before loop %s\n", tmp->content);
 	while (tmp /* && !token_is_operator(tmp) */)
 	{
 		if ((tmp->next == NULL) || (token_is_operator(tmp->next)))
 		{
-			
 			if (tmp->next)
 				word->next = tmp->next;
 			else
@@ -62,32 +66,44 @@ void	mv_word_left(t_token *all)
 			tmp->next = word;
 			break ;
 		}
-		/* if (token_is_operator(tmp))
-			break ; */
-		//previous = tmp;
-		//printf("previous %s\n", tmp->content);
 		tmp = tmp->next;
-		//printf("tmp %s\n", tmp->content);
-		
 	}
-	printf("------- AFTER -----------\n");
-	print_token(&all);
-	printf("------------------\n");
 }
 
-
-/*
-	cette fonction permet de bouger le CHV_L et le WORD suivant juste avant le prochain op ou fin de la cmd_line
-	attwntion on se situe un noed avant
-*/
-
-/* void	mv_redir_left(t_token *all)
+void	mv_chv_l(t_token *all)
 {
-	t_token *tmp;
 
+	t_token	*tmp = NULL;
+	t_token	*forward = NULL;
+	t_token	*word = NULL;
+
+	//all serra ici toujours le noead avant chv_l
 	tmp = all;
-
-} */
+	//foward pointe sur -l
+	forward = tmp->next->next;
+	// word sur ok
+	word = tmp->next;
+	// tmp est sur le chevron et on veut link le chevron et le WORD + 1 ex: < ok -l on link < et -l
+	//if (tmp->type != CHV_L)
+	// tmp = tmp->next;
+	// on link le chv_l et -l
+	tmp->next = forward;
+	//if (tmp-> type != CHV_L)
+	tmp = tmp->next->next;
+	while (tmp /* && !token_is_operator(tmp) */)
+	{
+		if ((tmp->next->next == NULL) || (token_is_operator(tmp->next->next)))
+		{
+			if (tmp->next)
+				word->next = tmp->next;
+			else
+				word->next = NULL;
+			tmp->next = word;
+			break ;
+		}
+		tmp = tmp->next;
+	}
+}
 
 /*
 	on cherche ici a resoudre le cas suivant wc < ok -l < salut  <=> wc -l < ok < salut . la regle consiste asi on rencontre un chevron
@@ -97,29 +113,38 @@ void	mv_word_left(t_token *all)
 */
 
 
-/* t_token	*remix_lexer(t_token *all)
+void	remix_lexer(t_token *all)
 {
+	printf("------- BEFORE -----------\n");
+	print_token(&all);
+	printf("------------------\n");
 	t_token	*tmp;
 	t_token	*before_chv_l;
-	t_token	*actual;
+	//t_token	*actual;
 
 	tmp = all;
 	// faire function si < en premier position de cmd_line
+	before_chv_l = all;
 	while (tmp)
 	{
 		if (tmp->type == CHV_L)
 		{
 				if (count_word_btw_two_op(tmp) > 1)
 				{
-
+					//printf("%s\n", tmp->content);
+					mv_word_left(tmp);
+					//mv_chv_l(before_chv_l);
 					//tmp = mv_redir_left(before_chv_l);
 					// on se retrouve apres substitution au noed avant lancienne position de chvr_l
 				}
 		}
-		tmp = before_chv_l;
+		before_chv_l = tmp;
 		tmp = tmp->next;
 	}
-} */
+	printf("------- AFTER -----------\n");
+	print_token(&all);
+	printf("------------------\n");
+}
 
 int	g_status;
 
@@ -135,9 +160,9 @@ int main(int ac, char **av, char **envp)
 	char *line = NULL;
 
 	env = ft_create_env(envp);
-	line = ft_strdup("wc < ok -l -c < ls");
+	line = ft_strdup("wc < ok -l < ls");
 	temp = lexer(line);
-	mv_word_left(temp);
+	remix_lexer(temp);
 
 	//ft_lstclear(&expanded, free);
 	//ft_cmd_clear(&cmd_all);
