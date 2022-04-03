ff7c20904abfc1ea03c634fec89319a5f3b83935
gustave mary <gustave1mary@gmail.com>
2022-04-03 15:37:01 +0200

    norming gus

--- srcs/expand_new/dollars_2.c
+++ srcs/expand_new/dollars_2.c
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 14:04:36 by gmary             #+#    #+#             */
-/*   Updated: 2022/04/03 15:27:58 by gmary            ###   ########.fr       */
+/*   Updated: 2022/04/03 15:33:58 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,13 +55,15 @@ char	*ft_allocate_dest(char *str, char *status, char *dest,
 	return (dest);
 }
 
-char	*replace_interrogation_end(char *str, char *status, char *var_name)
+char	*replace_interrogation_end(char *str, char *status,
+		char *var_name, char *dest)
 {
 	//WARNING status et str ETTTTTTT VAR_NAME 
 	free(status);
 	free(str);
 	if (var_name)
 		free(var_name);
+	return (dest);
 }
 
 char	*replace_interrogation(char *str, int pos, t_to_clean *clean,
@@ -91,6 +93,5 @@ char	*replace_interrogation(char *str, int pos, t_to_clean *clean,
 			replace_interrogation_ter(str, dest, &i, &k);
 	}
 	dest[k] = '\0';
-	replace_interrogation_end(str, status, var_name);
-	return (dest);
+	return (replace_interrogation_end(str, status, var_name, dest));
 }
